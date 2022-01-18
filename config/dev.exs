443149648d9b2c0b1306e8e62fcc8a0c99429572
Random <ibuildeveryday@protonmail.com>
2022-01-18 02:02:26 -0500

    Tailwind + mockup

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,15 @@ config :hyper, HyperWeb.Endpoint,
   secret_key_base: "V/4rlVMp4sXFYQoDMUaaSlTDCVn+sJvvq9/JKSBQ8LoYvNGMeNcKMInoApEjF0XM",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
